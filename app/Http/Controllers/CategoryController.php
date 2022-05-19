afd6350510aaaff381686d453445a67ea42caa83
HinterPress <50248789+HinterPress@users.noreply.github.com>
2022-05-19 18:33:07 +0400

    create back api category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,13 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
 
-    public function getCategory()
+    public function getCategory() //get data category
     {
-        $getData = DB::table('category')->where('rec_state', 0)->get();
-        return response()->json(['data' => $getData]);
+        try {
+            $statusCode = 200;
+            $statusMessage = "Success";
+            $getData = DB::table('category')->select('id', 'name')->where('rec_state', 0)->get();
+        } catch (\Throwable $th) {
+            $statusCode = 500;
+            $statusMessage = "Failed";
+            $getData = [];
+        }
+        return response()->json(['statusCode' => $statusCode, 'statusMessage' => $statusMessage, 'data' => $getData]);
+    }
+
+    public function insertCategory(Request $request)
+    {
+        $validRequest = Validator::make($request->all(), [
+            'name' => 'required|max:3'
+        ]);
+        if ($validRequest->fails()) {
+            return response()->json('test');
+        }
+        // return response()->json($validRequest->fails());
+
+        // if($validRequest){
+        //     return response()->json($validRequest);
+        // }else{
+        //     return response()->json('test', 500);
+        // }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/getCategory', [CategoryController::class, 'getCategory']);
+Route::post('/insertCategory', [CategoryController::class, 'insertCategory']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
