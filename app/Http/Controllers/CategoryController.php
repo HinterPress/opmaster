e4bae43a4b7dff33f5f4df1719a0beff4f3650d4
HinterPress <50248789+HinterPress@users.noreply.github.com>
2022-05-20 13:07:35 +0400

    create category contoller get data and insert data
    davamate category damateba da gamotana

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class CategoryController extends Controller
 {
@@ -23,20 +24,28 @@ class CategoryController extends Controller
         return response()->json(['statusCode' => $statusCode, 'statusMessage' => $statusMessage, 'data' => $getData]);
     }
 
-    public function insertCategory(Request $request)
+    public function insertCategory(Request $request) // crate category
     {
         $validRequest = Validator::make($request->all(), [
-            'name' => 'required|max:3'
+            'name' => 'required|max:255'
         ]);
-        if ($validRequest->fails()) {
-            return response()->json('test');
+        if ($validRequest->fails()) { //if validation true
+            $errorMessage = $validRequest->errors()->all();
+            $statusCode = 500;
+            $statusMessage = "Failed";
+        } else {
+            $name = $request->name;
+            try {
+                $statusCode = 200;
+                $statusMessage = "Success";
+                $errorMessage = [];
+                DB::table('category')->insert(['name' => $name]);
+            } catch (\Exception $th) {
+                $statusCode = 500;
+                $statusMessage = "Failed";
+                $errorMessage = $th->getMessage();
+            }
         }
-        // return response()->json($validRequest->fails());
-
-        // if($validRequest){
-        //     return response()->json($validRequest);
-        // }else{
-        //     return response()->json('test', 500);
-        // }
+        return response()->json(['statusCode' => $statusCode, 'statusMessage' => $statusMessage, 'errorMessage' => $errorMessage]);
     }
 }
